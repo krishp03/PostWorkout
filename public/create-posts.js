356fa48af09cd859463f35d09ca93ba1bf1b2acc
Krish <krpxbox@gmail.com>
2021-08-03 18:31:14 +0000

    checks cases

--- public/create-posts.js
+++ public/create-posts.js
@@ -1,5 +1,4 @@
 let googleUser;
-var check = true; 
 
 window.onload = (event) => {
     // Use this to retain user state between html pages.
@@ -19,79 +18,97 @@ $('#select').change(function () {
     if (opt == "Diet") {
         $('#msgbox').html('<br><label  class="ds">Protein: </label> <input placeholder = "Grams of protein" id = "protein" class="input is-medium" type="text" id="noteTitle"><br><label class="ds">Fat: </label> <input  placeholder = "Grams of Fat" id = "fat" class="input is-medium" type="text" id="noteTitle"><br><label class="ds">Carbs: </label> <input placeholder = "Grams of Carbs" id = "carbs" class="input is-medium" type="text" id="noteTitle">');
     } else if (opt == "Workout") {
-        check = false; 
         $('#msgbox').html('<br> <label  class="ds">Number of Sets: </label> <input  placeholder = "Number of sets" id = "sets" class="input is-medium" type="text" id="noteTitle">');
     } else if (opt == "0") {
         $('#msgbox').html('')
     }
 });
 
+const checkIfValid = () => {
+    const select = document.querySelector('#select');
+    if (select.selectedIndex === 0) {
+        alert("Select a category");
+    } else {
+        try {
+            document.querySelector('#noteTitle').value;
+            document.querySelector('#noteText').value;
+            if (select.selectedIndex === 2) {
+                document.querySelector('#sets').value;
+            }
+            else {
+                document.querySelector('#protein').value;
+                document.querySelector('#carbs').value;
+                document.querySelector('#fat').value;
+            }
+            handleNoteSubmit();
+        } catch (error) {
+            console.log(error);
+            alert("you left one(or more) fields empty")
+        }
+    }
+}
+
 const handleNoteSubmit = () => {
     // 1. Capture the form data
     const noteTitle = document.querySelector('#noteTitle');
     const noteText = document.querySelector('#noteText');
-    const noteImage = document.querySelector('#noteImage');
     var protein = document.querySelector('#protein')
     var carbs = document.querySelector('#carbs')
     var fat = document.querySelector('#fat')
     var sets = document.querySelector('#sets')
 
-    if(check === false){
+    if (select.selectedIndex === 2) {
         protein = ""
         carbs = ""
         fat = ""
         sets = sets.value
-        
+
     }
-    else{
+    else {
         sets = ""
         protein = protein.value
         fat = fat.value
         carbs = carbs.value
     }
-    const select = document.querySelector('#select');
     const noteLabel = select.options[select.selectedIndex];
     console.log(select.selectedIndex)
-    console.log(check)
 
 
     firebase.database().ref(`users/${googleUser.uid}`).push({
-            title: noteTitle.value,
-            text: noteText.value,
-            image: noteImage.value,
-            label: noteLabel.value, 
-            protein: protein,
-            carbs: carbs,
-            fat: fat,
-            sets: sets
-            })
+        title: noteTitle.value,
+        text: noteText.value,
+        image: noteImage.value,
+        label: noteLabel.value,
+        protein: protein,
+        carbs: carbs,
+        fat: fat,
+        sets: sets
+    })
 
-       
-    
+
+
 
         // 3. Clear the form so that we can write a new note
         .then(() => {
-            if(select.selectedIndex === 1){
-            protein.value = "";
-            carbs.value = "";
-            fat.value = "";
-            noteTitle.value = "";
-            noteText.value = "";
-            noteText.value = "";
-            select.selectedIndex = null;
+            if (select.selectedIndex === 1) {
+                protein.value = "";
+                carbs.value = "";
+                fat.value = "";
+                noteTitle.value = "";
+                noteText.value = "";
+                noteText.value = "";
+                select.selectedIndex = null;
             }
-            else if(select.selectedIndex === 2){
+            else if (select.selectedIndex === 2) {
                 sets.value = "";
                 noteTitle.value = "";
-            noteText.value = "";
-            noteText.value = "";
-            select.selectedIndex = null;
+                noteText.value = "";
+                noteText.value = "";
+                select.selectedIndex = null;
             }
-            
-            
+
+
         });
-$('#msgbox').html('')
+    $('#msgbox').html('')
 
 }
-
-
